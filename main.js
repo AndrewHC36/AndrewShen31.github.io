8ed0bd2c97f9e4579d246e814bc1e0ba5c7132eb
Andrew Shen <39780709+AndrewShen31@users.noreply.github.com>
2018-08-20 06:40:17 -0700

    Updated

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// constants
+var last_upd = "Last Update: Aug 19, 2018";
 
+function upd_constants() {
+  document.getElementById("last-update constants").innerHTML = last_upd;
+}
+
+
+/*
+function clockUpd() {
+  var today = new Date();
+  var date = today.getDate();
+  document.getElementById("clock-counter").innerHTML = date;
+}
+*/
