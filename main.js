65db7c47f73729102e42fef131a15b413ae48e19
Andrew Shen <39780709+AndrewShen31@users.noreply.github.com>
2018-08-20 08:50:51 -0700

    8202018-0850AM

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // constants
-var last_upd = "Last Update: Aug 19, 2018";
+var last_upd = "Last Update: Aug 20, 2018";
 
 function upd_constants() {
   document.getElementById("last-update constants").innerHTML = last_upd;
